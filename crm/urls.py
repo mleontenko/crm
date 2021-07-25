166dd6732b2c679abd09e74e3c36c76d94d4b118
Marin Leontenko <marin.leontenko@gmail.com>
2021-07-25 20:54:37 +0200

    signup

--- crm/urls.py
+++ crm/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
-from leads.views import landing_page, LandingPageView
+from leads.views import landing_page, LandingPageView, SignupView
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing-page'),
     path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls', namespace="leads")),   
+    path('leads/', include('leads.urls', namespace="leads")),
+    path('signup/', SignupView.as_view(), name='signup'),  
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout') 
 
