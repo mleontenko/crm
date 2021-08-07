e7b7b9b02533787ca1fedf28aaa3c7a215877a0e
Marin Leontenko <marin.leontenko@gmail.com>
2021-08-07 14:23:08 +0200

    signals for UserProfile

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
     pass
@@ -25,3 +26,9 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.user.username
+
+def post_user_createdsignal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_createdsignal, sender=User)
