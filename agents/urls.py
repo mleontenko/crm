0a03864bc4b6f3126f6a213195296830661f22f6
Marin Leontenko <marin.leontenko@gmail.com>
2021-08-18 18:39:31 +0200

    agent detail

--- agents/urls.py
+++ agents/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import AgentListView, AgentCreateView
+from .views import AgentListView, AgentCreateView, AgentDetailView
 
 app_name = 'agents'
 
 urlpatterns = [
     path('', AgentListView.as_view(), name='agent-list'),
+    path('<int:pk>/', AgentDetailView.as_view(), name='agent-detail'),
+    #path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
+    #path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
     path('create/', AgentCreateView.as_view(), name='agent-create')
 ]
