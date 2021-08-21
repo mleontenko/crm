05b6d47fefe3090c1aeef1364b2517b0ebb64c1c
Marin Leontenko <marin.leontenko@gmail.com>
2021-08-21 15:19:38 +0200

    agent mixin for leads

--- leads/views.py
+++ leads/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views import generic
 from .models import Lead, Agent
+from agents.mixins import OrganisorAndLoginRequiredMixin
 from .forms import LeadForm, LeadModelForm, CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -43,7 +44,7 @@ def lead_detail(request, pk):
     }
     return render(request, "leads/lead_detail.html", context)
 
-class LeadCreateView(LoginRequiredMixin, generic.CreateView):
+class LeadCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
@@ -72,7 +73,7 @@ def lead_create(request):
     }
     return render(request, "leads/lead_create.html", context)
 
-class LeadUpdateView(LoginRequiredMixin, generic.UpdateView):
+class LeadUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
@@ -95,7 +96,7 @@ def lead_update(request, pk):
     }
     return render(request, "leads/lead_update.html", context)
 
-class LeadDeleteView(LoginRequiredMixin, generic.DeleteView):
+class LeadDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
 
